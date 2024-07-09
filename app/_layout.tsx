e7df0689a095d396faaac7288c578baf2c59af61
Maxence Leroy <maxence.leroy3@gmail.com>
2024-07-09 20:30:34 +0200

    feat: create future culprit setting screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -31,6 +31,7 @@ export default function RootLayout() {
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(madame-chou)/index" options={{ headerShown: false }} />
+        <Stack.Screen name="culprit-setting/index" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
     </ThemeProvider>
